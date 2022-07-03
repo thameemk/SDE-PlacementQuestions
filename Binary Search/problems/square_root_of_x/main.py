16c1376c6c0602c0049967d4874e22e3604093e8
thameemk612 <thameemk612@gmail.com>
2022-07-03 15:50:22 +0530

    change: sqrt

--- Binary Search/problems/square_root_of_x/main.py
+++ Binary Search/problems/square_root_of_x/main.py
@@ -1,11 +1,30 @@
 """
 Link: https://leetcode.com/problems/sqrtx/
 """
+import math
 
 
-def sqrt(x: int):
-    pass
+def sqrt(x: int) -> int:
+    return math.floor(math.sqrt(x))
+
+
+def sqrt_conventional_method(x: int) -> int:
+    sqrt_found = False
+    sqrt = 1
+    while(sqrt_found == False):
+        current_square = sqrt*sqrt
+        if current_square == x:
+            sqrt_found = True
+        elif current_square > x:
+            sqrt_found = True
+            sqrt = sqrt - 1
+        else:
+            sqrt = sqrt + 1
+
+    return sqrt
 
 
 if __name__ == '__main__':
-    sqrt(x=8)
+    x = 8
+    r1, r2 = sqrt(x), sqrt_conventional_method(x)
+    print(r1, r2)
