c27724f9ee22f65a65a4c281a391975352c44383
thameemk612 <thameemk612@gmail.com>
2022-07-23 01:34:11 +0530

    add: climbing_stairs - python

--- Problems/climbing-stairs/main.py
+++ Problems/climbing-stairs/main.py
@@ -1,9 +1,24 @@
 
 
+def sum_of_fibonacci_series(num: int) -> int:
+    sum = 0
+    i = 1
+    while i > num:
+        sum = sum + i
+
+
 def climbing_stairs(n: int) -> int:
-    pass
+    if n == 1:
+        return 1
+
+    fibonacci_series = [1, 1]
+
+    for i in range(2, n+1):
+        fibonacci_series.append(fibonacci_series[i-1]+fibonacci_series[i-2])    
+
+    return fibonacci_series[-1]
 
 
 if __name__ == '__main__':
-    response = climbing_stairs(2)
+    response = climbing_stairs(4)
     print(response)
