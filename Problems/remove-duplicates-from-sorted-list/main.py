6b32a4f4f84fa8a52f80fb550315ac915bc9a5f1
thameemk612 <thameemk612@gmail.com>
2022-07-23 21:17:22 +0530

    add: delete_duplicates

---
+++ Problems/remove-duplicates-from-sorted-list/main.py
@@ -0,0 +1,15 @@
+"""
+Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+"""
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def delete_duplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
