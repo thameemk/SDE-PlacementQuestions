82ac269589b3edcb8d11ad7a2675056d9fb654d9
thameemk612 <thameemk612@gmail.com>
2022-09-03 00:15:59 +0530

    change: max_depth

--- Tree/maximum-depth-of-binary-tree/main.py
+++ Tree/maximum-depth-of-binary-tree/main.py
@@ -17,4 +17,32 @@ class TreeNode:
 class Solution:
 
     def max_depth(self, root: Optional[TreeNode]) -> int:
-        pass
+
+        if root is None:
+            return 0
+
+        return self._get_depth(root.left, root.right)
+
+    def _get_depth(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> int:
+
+        self.depth = 0
+
+        if left is None and right is None:
+            self.depth += 0
+
+        elif left is None:
+            self.depth += self._get_depth(right.left, right.right)
+
+        elif right is None:
+            self.depth += self._get_depth(left.left, left.right)
+
+        else:
+            _right_depth = self._get_depth(right.left, right.right)
+            _left_depth = self._get_depth(left.left, left.right)
+
+            if _right_depth > _left_depth:
+                self.depth += _right_depth
+            else:
+                self.depth += _left_depth
+
+        return self.depth
