5db7ab932cf620e4e8203d995ff822c8a7277a8c
thameemk612 <thameemk612@gmail.com>
2022-09-02 23:52:02 +0530

    add: max depth

---
+++ Tree/maximum-depth-of-binary-tree/main.py
@@ -0,0 +1,20 @@
+"""
+Problem Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+"""
+
+
+from typing import Optional
+
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def max_depth(self, root: Optional[TreeNode]) -> int:
+        pass
