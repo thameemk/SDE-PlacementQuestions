424103cf142881e6b4eca4908a8767e258f12fe2
thameemk612 <thameemk612@gmail.com>
2022-09-03 22:41:02 +0530

    add: balanced tree

---
+++ Tree/balanced-binary-tree/main.py
@@ -0,0 +1,18 @@
+"""
+Link: https://leetcode.com/problems/balanced-binary-tree/
+"""
+
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def is_balanced(self, root: Optional[TreeNode]) -> bool:
+        pass
