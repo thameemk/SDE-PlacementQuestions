e200acf283a1261c0e4c152a055fe0109fdace85
thameemk612 <thameemk612@gmail.com>
2022-09-04 00:08:19 +0530

    change: is_balanced

--- Tree/balanced-binary-tree/main.py
+++ Tree/balanced-binary-tree/main.py
@@ -3,6 +3,7 @@ Link: https://leetcode.com/problems/balanced-binary-tree/
 """
 
 
+from math import fabs
 from typing import Optional
 
 
@@ -15,4 +16,25 @@ class TreeNode:
 
 class Solution:
     def is_balanced(self, root: Optional[TreeNode]) -> bool:
-        pass
+
+        if root is None:
+            return True
+
+        self.balanced = True
+
+        self._get_height(root)
+
+        return self.balanced
+
+    def _get_height(self, root: Optional[TreeNode]) -> int:
+
+        if root is None:
+            return 0
+
+        _left_depth = self._get_height(root.left)
+        _right_depth = self._get_height(root.right)
+
+        if abs(_left_depth-_right_depth) > 1:
+            self.balanced = False
+
+        return max(_left_depth+1, _right_depth+1)
