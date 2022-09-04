027b5f044801e2d06e8f21eee0a8c218d5fd37ff
thameemk612 <thameemk612@gmail.com>
2022-09-04 22:59:30 +0530

    change: minimum depth of binary tree

--- Tree/minimum-depth-of-binary-tree/main.py
+++ Tree/minimum-depth-of-binary-tree/main.py
@@ -16,4 +16,27 @@ class TreeNode:
 
 class Solution:
     def min_depth(self, root: Optional[TreeNode]) -> int:
-        pass
+        if root is None:
+            return 0
+
+        return self._get_depth(root.left, root.right)+1
+
+    def _get_depth(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> int:
+
+        if left is None and right is None:
+            return 0
+
+        elif left is None:
+            return self._get_depth(right.left, right.right)+1
+
+        elif right is None:
+            return self._get_depth(left.left, left.right)+1
+
+        else:
+            _right_depth = self._get_depth(right.left, right.right)
+            _left_depth = self._get_depth(left.left, left.right)
+
+            if _right_depth > _left_depth:
+                return _left_depth+1
+            else:
+                return _right_depth+1
