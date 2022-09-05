f1963b205750f6727636a684fea4dd248b31b410
thameemk612 <thameemk612@gmail.com>
2022-09-06 00:14:16 +0530

    add: pascals_triangle

---
+++ Dynamic Programming/pascals-triangle/main.py
@@ -0,0 +1,12 @@
+"""
+Link: https://leetcode.com/problems/pascals-triangle/
+"""
+
+
+from typing import List
+
+
+class Solution:
+
+    def generate_pascals_triangle(self, num_rows: int) -> List[List[int]]:
+        pass
