db100d33f7f6a0abe319128fa2d9fedfe54d75a2
thameemk612 <thameemk612@gmail.com>
2022-09-06 00:49:53 +0530

    change: pascals-triangle

--- Dynamic Programming/pascals-triangle/main.py
+++ Dynamic Programming/pascals-triangle/main.py
@@ -2,11 +2,29 @@
 Link: https://leetcode.com/problems/pascals-triangle/
 """
 
-
 from typing import List
 
 
 class Solution:
 
     def generate_pascals_triangle(self, num_rows: int) -> List[List[int]]:
-        pass
+        pascals_triangle = [[1]]
+
+        if num_rows == 1:
+            return pascals_triangle
+
+        for i in range(1, num_rows):
+
+            pascals_triangle.append([1])
+
+            for j in range(1, i):
+                pascals_triangle[i].append(pascals_triangle[i -
+                                                            1][j] + pascals_triangle[i - 1][j - 1])
+
+            pascals_triangle[i].append(1)
+
+        return pascals_triangle
+
+
+if __name__ == '__main__':
+    print(Solution().generate_pascals_triangle(num_rows=5))
