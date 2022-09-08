7db42fd7fbbed44988680ad18fda701ac8b3ba3f
thameemk612 <thameemk612@gmail.com>
2022-09-08 23:12:56 +0530

    add: max-profit

---
+++ Dynamic Programming/max-profit/main.py
@@ -0,0 +1,23 @@
+#  Project : SDE Placement Questions
+#  Filename : main.py
+#  Author : blacklist
+#  Current modification time : Thu, 8 Sep 2022 at 10:32 pm India Standard Time
+#  Last modified time : Thu, 8 Sep 2022 at 10:32 pm India Standard Time
+
+"""
+Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+"""
+
+
+def max_profit(prices: list[int]) -> int:
+    maximum_profit = 0
+    for i in range(1, len(prices)):
+        current_profit = prices[i] - prices[i - 1]
+        if prices[i] > prices[i - 1]:
+            maximum_profit = max(current_profit, maximum_profit)
+
+    return maximum_profit
+
+
+if __name__ == '__main__':
+    print(max_profit(prices=[7, 1, 5, 3, 6, 4]))
