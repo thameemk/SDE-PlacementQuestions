cf943299b381df92011bf75b4d931d5a71f6c2e2
thameemk612 <thameemk612@gmail.com>
2022-09-12 23:52:36 +0530

    change: single_number

--- Arrays/single-number/main.py
+++ Arrays/single-number/main.py
@@ -8,6 +8,18 @@ from typing import List
 
 class Solution:
     def single_number(self, nums: List[int]) -> int:
-        pass
-        
+        map = {}
+        for num in nums:
+            if num in map:
+                map[num] = map[num]+1
+            else:
+                map[num] = 1
 
+        keys = list(map.keys())
+        values = list(map.values())
+
+        return keys[values.index(1)]
+
+
+if __name__ == '__main__':
+    print(Solution().single_number(nums=[2,2,1]))
