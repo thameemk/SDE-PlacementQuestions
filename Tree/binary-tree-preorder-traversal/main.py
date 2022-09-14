2fd7832aeec9b2d94f96c43e3267dedae171d8cf
thameemk612 <thameemk612@gmail.com>
2022-09-15 00:24:02 +0530

    add: preorder_traversal

---
+++ Tree/binary-tree-preorder-traversal/main.py
@@ -0,0 +1,18 @@
+"""
+Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+Topics: Stack, Tree, Depth-First Search, Binary Tree
+"""
+
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorder_traversal(self, root: Optional[TreeNode]) -> List[int]:
+        pass
