cdd4cc70b438b57e66575ad802af340d46f6550e
thameemk612 <thameemk612@gmail.com>
2022-09-26 23:03:16 +0530

    add: convert to title

---
+++ String/excel-sheet-column-title/main.py
@@ -0,0 +1,14 @@
+#  Project : SDE Placement Questions
+#  Filename : main.py
+#  Author : blacklist
+#  Current modification time : Mon, 26 Sep 2022 at 11:01 pm India Standard Time
+#  Last modified time : Mon, 26 Sep 2022 at 11:01 pm India Standard Time
+
+"""
+Link: https://leetcode.com/problems/excel-sheet-column-title/
+Tags: Math, String
+"""
+
+
+def convert_to_title(column_number: int) -> str:
+    pass
