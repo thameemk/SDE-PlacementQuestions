76ac296eac85bec3386739afe265d1bdbf5cf3ea
thameemk612 <thameemk612@gmail.com>
2022-10-06 22:39:16 +0530

    change: majority element

--- Arrays/majority-element/main.py
+++ Arrays/majority-element/main.py
@@ -8,3 +8,19 @@
 Link: https://leetcode.com/problems/majority-element/
 Tags: Array, Hash Table, Divide & Conquer, Sorting, Counting
 """
+from typing import List
+
+
+def majority_element(nums: List[int]) -> int:
+    set_nums = list(set(nums))
+
+    len_of_array = len(nums)
+
+    for num in set_nums:
+        no_of_occurrence = nums.count(num)
+        if no_of_occurrence > len_of_array/2:
+            return num
+
+
+if __name__ == '__main__':
+    print(majority_element([2, 2, 1, 1, 1, 2, 2]))
