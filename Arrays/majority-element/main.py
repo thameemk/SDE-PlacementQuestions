4df667c0eaa306a1340e01f00047f3e7a7d4af22
thameemk612 <thameemk612@gmail.com>
2022-10-06 22:33:06 +0530

    add: majority element

---
+++ Arrays/majority-element/main.py
@@ -0,0 +1,10 @@
+#  Project : SDE Placement Questions
+#  Filename : main.py
+#  Author : thameem
+#  Current modification time : Thu, 6 Oct 2022 at 10:27 pm India Standard Time
+#  Last modified time : Thu, 6 Oct 2022 at 10:26 pm India Standard Time
+
+"""
+Link: https://leetcode.com/problems/majority-element/
+Tags: Array, Hash Table, Divide & Conquer, Sorting, Counting
+"""
