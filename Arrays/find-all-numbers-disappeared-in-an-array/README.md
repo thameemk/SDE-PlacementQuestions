d7199a5c11b3ee582cffeaad656b3ff5f356b5ad
thameemk612 <thameemk612@gmail.com>
2022-10-29 23:21:47 +0530

    Add: Find All Numbers Disappeared in an Array

---
+++ Arrays/find-all-numbers-disappeared-in-an-array/README.md
@@ -0,0 +1,5 @@
+### Find All Numbers Disappeared in an Array
+
+Link: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+Tags: Array, Hash Table
---
+++ Arrays/find-all-numbers-disappeared-in-an-array/find_disappeared_numbers.py
@@ -0,0 +1,22 @@
+
+
+from typing import List
+
+
+def find_disappeared_numbers(nums: List[int]) -> List[int]:
+
+
+    res = []
+
+    for i in range(1,len(nums)+1):
+        if i not in nums:
+            res.append(i)
+
+    return res
+
+
+if __name__ == '__main__':
+
+    print(find_disappeared_numbers([4, 3, 2, 7, 8, 2, 3, 1]))
+    print(find_disappeared_numbers([4, 3, 2, 7, 7, 2, 3, 1]))
+    print(find_disappeared_numbers([1,1]))
