e796df3e4dfa5a513d5e2faf3d1018ee19bcd000
thameemk612 <thameemk612@gmail.com>
2022-11-06 15:16:29 +0530

    add: binary_tree_paths - cpp

---
+++ Tree/binary-tree-paths/binary_tree_paths.py
@@ -0,0 +1,17 @@
+#  Project : SDE Placement Questions
+#  Filename : binary_tree_paths.py
+#  Author : thameem
+#  Current modification time : Sat, 5 Nov 2022 at 9:28 pm India Standard Time
+#  Last modified time : Sat, 5 Nov 2022 at 9:28 pm India Standard Time
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def binary_tree_paths(root: Optional[TreeNode]) -> List[str]:
+    pass
