1186b7bf2ce585f1104e7952ab653224c2f7a5df
thameemk612 <thameemk612@gmail.com>
2022-12-12 23:54:14 +0530

    Add: Island Perimeter

---
+++ Arrays/island-perimeter/README.md
@@ -0,0 +1,5 @@
+## Island Perimeter
+
+Link: https://leetcode.com/problems/island-perimeter/
+
+Tags: Array, Depth First Search, Breadth First Search, Matrix
---
+++ Arrays/island-perimeter/island_perimeter.py
@@ -0,0 +1,5 @@
+#  Project : SDE Placement Questions
+#  Filename : island_perimeter.py
+#  Author : thameem
+#  Current modification time : Mon, 12 Dec 2022 at 11:53 pm India Standard Time
+#  Last modified time : Mon, 12 Dec 2022 at 11:53 pm India Standard Time
