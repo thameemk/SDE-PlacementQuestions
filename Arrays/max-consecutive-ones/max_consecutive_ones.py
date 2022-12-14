ac1c6f97c6ad1fbc0fd012ba3368cd6e5bb668e5
thameemk612 <thameemk612@gmail.com>
2022-12-14 22:44:52 +0530

    Change: Max Consecutive Ones - Python

--- Arrays/max-consecutive-ones/max_consecutive_ones.py
+++ Arrays/max-consecutive-ones/max_consecutive_ones.py
@@ -3,3 +3,25 @@
 #  Author : thameem
 #  Current modification time : Wed, 14 Dec 2022 at 12:27 am India Standard Time
 #  Last modified time : Wed, 14 Dec 2022 at 12:27 am India Standard Time
+from typing import List
+
+
+def find_max_consecutive_ones(nums: List[int]) -> int:
+    max_consecutive = 0
+
+    temp = 0
+
+    for i in nums:
+        if i == 1:
+            temp += 1
+        else:
+            max_consecutive = max(temp, max_consecutive)
+            temp = 0
+
+    return max(temp, max_consecutive)
+
+
+if __name__ == '__main__':
+    print(find_max_consecutive_ones([1, 0, 1, 1, 0, 1]))
+    print(find_max_consecutive_ones([1, 1, 0, 1, 1, 1]))
+    print(find_max_consecutive_ones([0]))
