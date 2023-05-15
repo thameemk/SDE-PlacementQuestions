42a74c1b52d01418727a81e0bd69b75cfde4a59f
thameemk <thameemk612@gmail.com>
2023-05-15 21:37:26 +0530

    Add: 3sum

---
+++ Arrays/3sum/3_sum.py
@@ -0,0 +1,36 @@
+#  Project : SDE Placement Questions
+#  Filename : 3_sum.py
+#  Author : thameem
+#  Current modification time : Mon, 15 May 2023 at 9:25 pm India Standard Time
+#  Last modified time : Mon, 15 May 2023 at 9:25 pm India Standard Time
+
+
+def three_sum(nums: list[int]) -> list[list[int]]:
+    len_of_array = len(nums)
+
+    nums.sort()
+
+    result = set()
+
+    for i in range(len_of_array):
+        left, right = i + 1, len_of_array - 1
+
+        while left < right:
+
+            sum_of_elements = nums[i] + nums[left] + nums[right]
+
+            if sum_of_elements == 0:
+                result.add((nums[i], nums[left], nums[right]))
+                left += 1
+                right -= 1
+
+            elif sum_of_elements < 0:
+                left += 1
+            else:
+                right -= 1
+
+    return list(result)
+
+
+if __name__ == '__main__':
+    print(three_sum([-1, 0, 1, 2, -1, -4]))
